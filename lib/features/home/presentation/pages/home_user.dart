71ca8a62b8d0ec3e5fa79159bff8421f534fae78
Eldengawy <eldengaawy@gmail.com>
2025-06-19 12:58:23 +0300

    refactor(home): replace static parking list with bloc state management
    Use BlocProvider and BlocBuilder to manage parking data state
    Add loading state handling for parkings data

--- lib/features/home/presentation/pages/home_user.dart
+++ lib/features/home/presentation/pages/home_user.dart
@@ -1,16 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:must_invest/config/routes/routes.dart';
 import 'package:must_invest/core/extensions/num_extension.dart';
 import 'package:must_invest/core/extensions/text_style_extension.dart';
 import 'package:must_invest/core/extensions/theme_extension.dart';
+import 'package:must_invest/core/services/di.dart';
 import 'package:must_invest/core/static/app_assets.dart';
 import 'package:must_invest/core/theme/colors.dart';
 import 'package:must_invest/core/translations/locale_keys.g.dart';
 import 'package:must_invest/core/utils/widgets/adaptive_layout/custom_layout.dart';
+import 'package:must_invest/core/utils/widgets/loading/loading_widget.dart';
 import 'package:must_invest/core/utils/widgets/long_press_effect.dart';
 import 'package:must_invest/features/explore/data/models/parking.dart';
+import 'package:must_invest/features/explore/presentation/cubit/explore_cubit.dart';
 import 'package:must_invest/features/home/presentation/widgets/home_user_header_widget.dart';
 import 'package:must_invest/features/home/presentation/widgets/my_points_card.dart';
 import 'package:must_invest/features/home/presentation/widgets/parking_widget.dart';
@@ -75,16 +79,35 @@ class _HomeUserState extends State<HomeUser> {
             height:
                 MediaQuery.of(context).size.height *
                 0.35, // Set a fixed height for the ListView
-            child: ListView.separated(
-              physics:
-                  const BouncingScrollPhysics(), // Add physics for better scrolling
-              shrinkWrap: false, // Don't use shrinkWrap as we've set a height
-              padding: EdgeInsets.zero, // Remove padding to avoid extra space
-              itemCount: parkingList.length,
-              separatorBuilder: (context, index) => const SizedBox(height: 16),
-              itemBuilder: (context, index) {
-                return ParkingCard(parking: parkingList[index]);
-              },
+            child: BlocProvider(
+              create:
+                  (BuildContext context) =>
+                      ExploreCubit(sl())..getAllParkings(),
+              child: BlocBuilder<ExploreCubit, ExploreState>(
+                builder: (BuildContext context, ExploreState state) {
+                  if (state is ParkingsLoading) {
+                    return LoadingWidget();
+                  } else if (state is ParkingsSuccess) {
+                    return ListView.separated(
+                      physics:
+                          const BouncingScrollPhysics(), // Add physics for better scrolling
+                      shrinkWrap:
+                          false, // Don't use shrinkWrap as we've set a height
+                      padding:
+                          EdgeInsets
+                              .zero, // Remove padding to avoid extra space
+                      itemCount: state.parkings.length,
+                      separatorBuilder:
+                          (context, index) => const SizedBox(height: 16),
+                      itemBuilder: (context, index) {
+                        return ParkingCard(parking: state.parkings[index]);
+                      },
+                    );
+                  } else {
+                    return SizedBox.shrink();
+                  }
+                },
+              ),
             ),
           ),
           30.gap, // Add some padding at the bottom
