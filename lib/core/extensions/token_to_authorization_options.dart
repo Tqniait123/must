9ebee86c06f322c1b99103695139950f9c9c0ee6
Eldengawy <eldengaawy@gmail.com>
2025-08-27 11:59:44 +0300

    fix: handle empty token in authorization options extension
    Add null check to prevent adding Authorization header when token is empty

--- lib/core/extensions/token_to_authorization_options.dart
+++ lib/core/extensions/token_to_authorization_options.dart
@@ -2,6 +2,9 @@ import 'package:dio/dio.dart';
 
 extension StringToAuthorizationOptions on String {
   Options toAuthorizationOptions() {
+    if (isEmpty) {
+      return Options();
+    }
     return Options(headers: {'Authorization': 'Bearer $this'});
   }
 }
