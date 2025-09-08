8c0bcc75aee4fe98ab1dbee8947e8cb13b43bbf2
Eldengawy <eldengaawy@gmail.com>
2025-09-08 12:57:28 +0300

    build(android): update minSdk and kotlin plugin version
    Update minSdk to use flutter.minSdkVersion for consistency with flutter configuration
    Upgrade kotlin android plugin to version 2.1.0 for latest features and bug fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tqniait.mustUser"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
